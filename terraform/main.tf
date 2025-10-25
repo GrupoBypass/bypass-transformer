b7f094b46d607b65bd1a829f7b99b171715a762b
gba <125586278+gba-exe@users.noreply.github.com>
2025-10-25 11:15:05 -0300

    Add HTTP ingress rule for port 5000
    Add ingress rule for HTTP traffic on port 5000

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,6 +22,14 @@ resource "aws_security_group" "ssh_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "Server"
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "http"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0

c00069ea34d3f4f4cb1ff529ac48a6256d7743ad
gba-exe <125586278+gba-exe@users.noreply.github.com>
2025-10-25 12:27:48 -0300

    feat(): dynamo

--- terraform/main.tf
+++ terraform/main.tf
@@ -74,6 +74,155 @@ resource "aws_instance" "transformer" {
   }
 }
 
+resource "aws_dynamodb_table" "circuito" {
+  name         = "Circuito"
+  billing_mode = "PAY_PER_REQUEST"   # equivalente ao modo on-demand
+  hash_key     = "sensor_id"
+
+  attribute {
+    name = "sensor_id"
+    type = "S"
+  }
+
+  table_class = "STANDARD"
+
+  tags = {
+    Environment = "prod"
+  }
+}
+
+resource "aws_dynamodb_table" "sensor_metadata" {
+  name         = "SensorMetadata"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "sensor_id"
+
+  attribute {
+    name = "sensor_id"
+    type = "S"
+  }
+
+  table_class = "STANDARD"
+
+  tags = {
+    Environment = "prod"
+  }
+}
+
+resource "aws_dynamodb_table" "piezo_sensor_distancia" {
+  name         = "PiezoSensorDistancia"
+  billing_mode = "PAY_PER_REQUEST"   # equivalente ao modo on-demand
+  hash_key     = "sensor_id"
+
+  attribute {
+    name = "sensor_id"
+    type = "S"
+  }
+
+  table_class = "STANDARD"
+
+  tags = {
+    Environment = "prod"
+  }
+}
+
+resource "aws_dynamodb_table" "tof_data" {
+  name         = "TofData"
+  billing_mode = "PAY_PER_REQUEST" # modo on-demand
+  hash_key     = "sensor_id"
+  range_key    = "datahora"
+
+  attribute {
+    name = "sensor_id"
+    type = "S"
+  }
+
+  attribute {
+    name = "datahora"
+    type = "S"
+  }
+
+  table_class = "STANDARD"
+
+  tags = {
+    Environment = "prod"
+  }
+}
+
+resource "aws_dynamodb_table" "piezo_data" {
+  name         = "PiezoData"
+  billing_mode = "PAY_PER_REQUEST" # modo on-demand
+  hash_key     = "trem_id"
+
+  attribute {
+    name = "trem_id"
+    type = "S"
+  }
+
+  table_class = "STANDARD"
+
+  tags = {
+    Environment = "prod"
+  }
+}
+
+resource "aws_dynamodb_table" "dps_data" {
+  name         = "DpsData"
+  billing_mode = "PAY_PER_REQUEST" # modo on-demand
+  hash_key     = "sensor_id"
+  range_key    = "datahora"
+
+  attribute {
+    name = "sensor_id"
+    type = "S"
+  }
+
+  attribute {
+    name = "datahora"
+    type = "S"
+  }
+
+  table_class = "STANDARD"
+
+  tags = {
+    Environment = "prod"
+  }
+}
+
+resource "aws_dynamodb_table" "dht11_data" {
+  name         = "DHT11Data"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "sensor_id"
+  range_key    = "datahora"
+
+  attribute {
+    name = "sensor_id"
+    type = "S"
+  }
+
+  attribute {
+    name = "datahora"
+    type = "S"
+  }
+
+  table_class = "STANDARD"
+
+  tags = {
+    Environment = "prod"
+  }
+}
+
+output "aws_dynamodb_table_circuito_name" {
+  value = aws_dynamodb_table.circuito.name
+}
+
+output "aws_dynamodb_table_sensor_metadata_name" {
+  value = aws_dynamodb_table.sensor_metadata.name
+}
+
+output "aws_dynamodb_table_piezo_sensor_distancia_name" {
+  value = aws_dynamodb_table.piezo_sensor_distancia.name
+}
+
 output "raw_bucket_name" {
   value = aws_s3_bucket.raw.bucket
 }

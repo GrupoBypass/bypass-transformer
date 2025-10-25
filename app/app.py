bc53cf6b98100e4f54044d293e50730ea7a91d28
gba-exe <125586278+gba-exe@users.noreply.github.com>
2025-10-25 16:26:18 -0300

    fix(): typo

--- app/app.py
+++ app/app.py
@@ -42,7 +42,7 @@ LOCAL_INPUT_PIEZO = "/tmp/input_piezo.csv"
 LOCAL_INPUT_OPTICAL = "/tmp/input_optical.csv"
 
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_SECRET_ACCESS_KEY_ID = os.environ.get("AWS_SECRET_ACCESS_KEY_ID")
 AWS_SESSION_TOKEN = os.environ.get("AWS_SESSION_TOKEN")
 
 session = boto3.Session(

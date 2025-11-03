70fe50537dd32d18539fc9c0d6203093c957d73b
gba-exe <125586278+gba-exe@users.noreply.github.com>
2025-11-03 19:28:13 -0300

    feat(): added dynamic state backend

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,6 +2,10 @@ variable "aws_region" {
   default = "us-east-1"
 }
 
+variable "state_bucket_name" {
+  default = "bypass-state-bucket"
+}
+
 variable "bucket_name" {
   default = "bypass-bucket"
 }

13f42ff53b73755eef4626b9648e813ff4217472
gba-exe <125586278+gba-exe@users.noreply.github.com>
2025-11-06 14:37:48 -0300

    feat(): bucket events

--- terraform/lambda/main.tf
+++ terraform/lambda/main.tf
@@ -49,3 +49,21 @@ resource "aws_lambda_function" "lambda_function" {
   source_code_hash = filebase64sha256(data.archive_file.lambda_zip.output_path)
 }
 
+resource "aws_lambda_permission" "allow_s3" {
+  statement_id  = "AllowS3Invoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda_function.lambda_handler
+  principal     = "s3.amazonaws.com"
+  source_arn    = data.terraform_remote_state.bypass_transformer.outputs.raw_bucket_arn
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = data.terraform_remote_state.bypass_transformer.outputs.raw_bucket_id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.lambda_function.arn
+    events              = ["s3:ObjectCreated:*"]
+  }
+
+  depends_on = [aws_lambda_permission.allow_s3]
+}
--- terraform/transformer/main.tf
+++ terraform/transformer/main.tf
@@ -293,6 +293,10 @@ output "raw_bucket_arn" {
   value = aws_s3_bucket.raw.arn
 }
 
+output "raw_bucket_id" {
+  value = aws_s3_bucket.raw.id
+}
+
 output "trusted_bucket_arn" {
   value = aws_s3_bucket.trusted.arn
 }
